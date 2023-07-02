b58d0a38f509f46963cb479a79aef66216afca58
Thelma-Dev <osifoaisosa@gmail.com>
2023-07-01 23:12:26 -0500

    Updated Factory Validation

--- assets/script/index.js
+++ assets/script/index.js
@@ -30,47 +30,48 @@ onEvent('click', create, function() {
     let shapeValue = shapeOption.options[shapeOption.selectedIndex].value;
     let colorOption = getElement("color");
     let colorValue = colorOption.options[colorOption.selectedIndex].value;
-    let htmlShape = document.createElement('div');
-    let shape = new Shape(shapeValue, colorValue);
 
-    // To validate limited number of shapes created after each push
-    if(validateLimit()) {
-        array.push(shape);
-    }        
+    if(shapeValue !== "" && colorValue !=="") {
+        let htmlShape = document.createElement('div');
+        let shape = new Shape(shapeValue, colorValue);    
 
 
-    // To create the shape based on selected option
-   if(shapeValue === 'circle') {
-    htmlShape.classList.add("circle"); 
-   } else {
-    htmlShape.classList.add("square"); 
-   }
+        // To create the shape based on selected option
+        shapeValue === 'circle' ? htmlShape.classList.add("circle"): htmlShape.classList.add("square"); 
+    
 
-    switch (colorValue) {
-        case "blue":
-            htmlShape.style.backgroundColor = '#09f';
-            break; 
-        case "green":
-            htmlShape.style.backgroundColor = '#9f0';
-            break;
-        case "orange":
-            htmlShape.style.backgroundColor = '#f90';
-            break;
-        case "purple":
-            htmlShape.style.backgroundColor = '#90f';
-            break;
-        case "pink":
-            htmlShape.style.backgroundColor = '#f09';
-    } 
-    factory.append(htmlShape);     // Append the shape to the factory
-    printInfo(htmlShape, shape);
-    return shape;
+        switch (colorValue) {
+            case "blue":
+                htmlShape.style.backgroundColor = '#09f';
+                break; 
+            case "green":
+                htmlShape.style.backgroundColor = '#9f0';
+                break;
+            case "orange":
+                htmlShape.style.backgroundColor = '#f90';
+                break;
+            case "purple":
+                htmlShape.style.backgroundColor = '#90f';
+                break;
+            case "pink":
+                htmlShape.style.backgroundColor = '#f09';
+        } 
+
+        // To validate limited number of shapes created after each push
+        if(validateLimit()) {
+            array.push(shape);
+            factory.append(htmlShape);     // Append the shape to the factory
+        }   
+       
+        printInfo(htmlShape, shape);
+        return shape;
+    }
 });
 
 function validateLimit() {
 
-    let numberOfShapes = factory.childElementCount
-    if(numberOfShapes === 14) {
+    //let numberOfShapes = factory.childElementCount
+    if(array.length === 15) {
         create.disabled = true;
         output.innerText = 'Storage is full!';
         return false;
